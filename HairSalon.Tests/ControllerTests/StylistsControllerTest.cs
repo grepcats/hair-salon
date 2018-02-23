871de761604db97f03da32e11763ef7aa51f06d8
Kayla Ondracek <kayla.renee@gmail.com>
2018-02-23 12:47:04 -0800

    add red controller view test for whether index returns right model type

--- HairSalon.Tests/ControllerTests/StylistsControllerTest.cs
+++ HairSalon.Tests/ControllerTests/StylistsControllerTest.cs
@@ -22,5 +22,18 @@ namespace HairSalon.Tests
             //assert
             Assert.IsInstanceOfType(result, typeof(ViewResult));
         }
+
+        [TestMethod]
+        public void Index_HasCorrectModelType_True()
+        {
+            //arrange
+            ViewResult indexView = new StylistsController().Index() as ViewResult;
+
+            //act
+            var result = indexView.ViewData.Model;
+
+            //assert
+            Assert.IsInstanceOfType(result, typeof(List<Stylist>));
+        }
     }
 }
